b91bee05cc6da25fb80f85b40d989f5b05aea608
Eduardo Fiorezi <eduardofiorezi@gmail.com>
2014-01-23 23:06:29 -0200

    Add youtube images to index

--- _site/assets/js/js.js
+++ _site/assets/js/js.js
@@ -1,33 +1,24 @@
-﻿        jQuery(document).ready(function() {
+﻿jQuery(document).ready(function() {
 
-            $("ul.list-fotos-box").simplecarousel({
-				next: $('.fPrevious'),
-				prev: $('.fNext'),
-                width:176,
-                height:134,
-				visible: 4,
-                fade: 400,
-                pagination: true
-            });
-			
-            $("ul.list-videos-box").simplecarousel({
-				next: $('.vPrevious'),
-				prev: $('.vNext'),
-                width:176,
-                height:134,
-				visible: 4,
-                fade: 400,
-                pagination: true
-            });
-			
-            $("ul.list-fotos-box-horario").simplecarousel({
-				next: $('.hPrevious'),
-				prev: $('.hNext'),
-				vertical: true,
-                width:164,
-                height:134,
-				visible: 5,
-                fade: 400,
-                pagination: false
-            });
-        });
+    $("ul.list-fotos-box, ul.list-videos-box").simplecarousel({
+      next: $('.fPrevious'),
+      prev: $('.fNext'),
+      width:176,
+      height:134,
+      visible: 4,
+      fade: 400,
+      pagination: true
+    });
+
+
+    $("ul.list-fotos-box-horario").simplecarousel({
+      next: $('.hPrevious'),
+      prev: $('.hNext'),
+      vertical: true,
+      width:164,
+      height:134,
+      visible: 5,
+      fade: 400,
+      pagination: false
+    });
+});
